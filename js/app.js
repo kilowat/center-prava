4276831166956059af0880ba6ceabb63fcb84003
kilowat <kadantsev.sergey@gmail.com>
2015-11-12 23:30:23 +0300

    init

--- js/app.js
+++ js/app.js
@@ -4,15 +4,20 @@ c_p= function(){
 			var q = innerWidth/500;
 			var sHeadW = $('.slide-head').width();
 			var sSeeW = $('.see-detail').width();
-			var w = window.innerWidth;
+				var w = window.innerWidth;
 			var wCenter = $('header .container').width()/2;
 			var wContainer = $('.wrapper').width();
-			$('.slide-head').css('left',w/2-wContainer/2-33 +'px');
+			var headLeft;
+			if(innerWidth<1050)
+				headLeft = -5;
+			else
+				headLeft = 40;
+			$('.slide-head').css('left',w/2-wContainer/2-headLeft +'px');
 			$('.see-detail').css('left',w/2-wContainer/2+60 +'px');
 			$('.slide-description').css('left',w/2-wContainer/2+90 +'px');
-			$('.swiper-button-next').css('right',w/2-wContainer/2+68 +'px');
-			$('.swiper-button-prev').css('right',w/2-wContainer/2+'px');
-			$('.swiper-pagination').css('left',w/2+'px');
+			$('.swiper-button-next').css('right',w/2-wContainer/2+58 +'px');
+			$('.swiper-button-prev').css('right',w/2-wContainer/2-10+'px');
+			$('.swiper-pagination').css('left',w/2-10+'px');
 			$('.swiper-container').height(window.innerWidth/q);	
 		},
 		miniSliderResize:function(){
